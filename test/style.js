5572bc9515e2dbf0ad123e51639ebd0b0859a151
Thorsten Lorenz <thlorenz@gmx.de>
2013-01-12 19:47:09 -0500

    correctly styling code also when inside <li>

--- test/style.js
+++ test/style.js
@@ -13,6 +13,7 @@ var styleSheet = {
   , h3 : 'underline blue'
   , h4 : 'underline'
   , h5 : 'underline brightBlack'
+  , code: 'bgWhite black'
   , parent : {
       li: 'brightBlack'
     }
@@ -22,6 +23,7 @@ function check(html, expected) {
   test('when parsing\n' + html + '\nand styling it, it renders as\n' + expected, function (t) {
     parse(html, function (err, res) {
       var styled = render(style(res, { styleSheet: styleSheet }))
+      console.log(styled)
       t.equals(styled, expected, 'correctly styled and rendered')
       t.end()
     })
@@ -50,3 +52,19 @@ test('headings', function (t) {
   t.end()
 })
 
+test('code inside <p>', function (t) {
+  var html = '<p>Here is some code: <code>var a = 3;</code></p>'
+    , expected = 'Here is some code: \u001b[30m\u001b[47mvar a = 3;\u001b[49m\u001b[39m'
+  check(html, expected)
+  t.end()
+});
+
+test('code inside <li><p>', function (t) {
+  var html = '<li><p>Here is some code: <code>var a = 3;</code> that you should read</p></li>'
+    , expected = 
+        '\u001b[90mHere is some code: \u001b[39m'
+      + '\u001b[30m\u001b[47mvar a = 3;\u001b[49m\u001b[39m'
+      + '\u001b[90m that you should read\u001b[39m'
+  check(html, expected)
+  t.end()
+});
